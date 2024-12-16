6d7c85da498fd9ad82ebaf2d85ca66b9070e482d
Olyxz16 <cedric.sazos@tutanota.com>
2024-12-16 14:31:06 +0100

    feat: added clipboard copy button

--- front/src/components/CopyButton.vue
+++ front/src/components/CopyButton.vue
@@ -1,9 +1,16 @@
 <script setup lang="ts">
   import { Button } from '@/components/ui/button'
+  import { useWorkstationStore } from '@/stores/workstationStore'
+
+  const wsstore = useWorkstationStore();
+  const copyToClipboard = () => {
+    const text = wsstore.fileContent();
+    navigator.clipboard.writeText(text);
+  }
 </script>
 
 <template>
-  <Button>
+  <Button @click="copyToClipboard">
     <p> copy </p>
   </Button>
 </template>
--- front/src/components/EnvTextfield.vue
+++ front/src/components/EnvTextfield.vue
@@ -1,10 +1,18 @@
 <script setup lang="ts">
+import { ref } from 'vue';
 import { Textarea } from "@/components/ui/textarea"
+import { useWorkstationStore } from '@/stores/workstationStore'
+
 const placeholder = "HOST=\nPORT=";
+
+const wsstore = useWorkstationStore();
+const updateStore = (payload : string | number) => {
+  wsstore.updateFileContent(payload)
+}
 </script>
 
 <template>
-    <Textarea class="textarea" :placeholder="placeholder"/>
+    <Textarea class="textarea" @update:modelValue="updateStore" :placeholder="placeholder"/>
 </template>
 
 <style scoped>
