a95af061430da269e623cdd994a07b0b8156e235
Cédric Sazos <cedric.sazos@tutanota.com>
2024-12-16 17:56:10 +0100

    feat: added repository to store

--- front/src/components/RepositorySelector.vue
+++ front/src/components/RepositorySelector.vue
@@ -1,11 +1,18 @@
 <script setup lang="ts">
 import { ComboBox } from '@/components/ui/combobox'
+import { useWorkstationStore } from '@/stores/workstationStore'
+
 const options = [
     { value: 'go-vue-template', label: 'go-vue-template' },
     { value: 'adoutchquizz', label: 'Adoutchquizz' }
 ];
 const placeholder="Select repository...";
 const searchPlaceholder="Search repository";
+
+const store = useWorkstationStore();
+function updateCurrentFile(name: string) {
+  store.setCurrentRepo(name);
+}
 </script>
 
 <template>
