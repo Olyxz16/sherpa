7c904a9909fbad5b30697a87c569d785ce2ee0b2
Olyxz16 <cedric.sazos@tutanota.com>
2024-12-22 14:31:52 +0100

    front: added repository support

--- front/src/components/RepositorySelector.vue
+++ front/src/components/RepositorySelector.vue
@@ -1,17 +1,26 @@
 <script setup lang="ts">
+import { ref, watch } from 'vue'
+import { storeToRefs } from 'pinia'
 import { ComboBox } from '@/components/ui/combobox'
+import type { Option } from '@/types/ComboBoxTypes'
 import { useWorkstationStore } from '@/stores/workstationStore'
+const wsStore = useWorkstationStore();
+import { useUserStore } from '@/stores/userStore'
+const userStore = useUserStore()
 
-const options = [
-    { value: 'go-vue-template', label: 'go-vue-template' },
-    { value: 'adoutchquizz', label: 'Adoutchquizz' }
-];
 const placeholder="Select repository...";
 const searchPlaceholder="Search repository";
+const options = ref<Option[]>([]);
 
-const store = useWorkstationStore();
+const { loggedIn, repositories } = storeToRefs(userStore)
+watch(loggedIn, () => {
+  options.value = repositories.value.map((repoName) => {
+    return {value: repoName.toLowerCase() , label: repoName}
+  })
+})
+
 function updateCurrentFile(name: string) {
-  store.setCurrentRepo(name);
+  wsStore.setCurrentRepo(name);
 }
 </script>
 
