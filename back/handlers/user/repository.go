6d60107456d991b62e999e8e21581ea73cb48d06
Cédric Sazos <cedric.sazos@tutanota.com>
2025-01-06 00:03:07 +0100

    feat: add user repo handler

---
+++ back/handlers/user/repository.go
@@ -0,0 +1,39 @@
+package user
+
+import (
+	"encoding/json"
+
+	"github.com/labstack/echo/v4"
+
+	"github.com/Olyxz16/go-vue-template/database"
+)
+
+
+func FetchUserRepoFiles(c echo.Context) error {
+    cookie, err := c.Cookie("session")
+    if err != nil {
+        return c.JSON(401, `{message: "Unauthorized"}`)
+    }
+    repoName := c.QueryParam("repo")
+    if repoName == "" {
+        return c.JSON(400, `{message: "Missing repository name"}`)
+    }
+    fileName := c.QueryParam("file")
+    if fileName == "" {
+        return c.JSON(400, `{message: "Missing file name"}`)
+    }
+    
+    content, err := database.FetchFile(cookie, repoName, fileName)
+    if err != nil {
+        // handle different errors
+        return c.JSON(500, `{message: "Missing data"}`)
+    }
+
+    response := make(map[string]interface{})
+    response["content"] = content
+    json, err := json.Marshal(response)
+    if err != nil {
+        return c.JSON(500, `{message: "Error fetch file"}`)
+    }
+    return c.JSON(200, json)
+}
